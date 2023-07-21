c466652ba0956f07bd3f2b472ee6e1cbc58c8f30
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-07-21 11:28:52 +0100

    Players array created with 2 players and name, surface & weather ratings added

---
+++ tennis.php
@@ -0,0 +1,6 @@
+<?php
+
+$players = [
+    ['name' => 'Andy Murray', 'surfaceRating' => ['lawnRating' => 8, 'clayRating' => 6, 'hardRating' => 5], 'weatherRating' => ['rainRating' => 7, 'dryRating' => 8, 'snowRating' => 6, 'sunnyRating' => 8]],
+    ['name' => 'Roger Federer', 'surfaceRating' => ['lawnRating' => 10, 'clayRating' => 8, 'hardRating' => 9], 'weatherRating' => ['rainRating' => 8, 'dryRating' => 8, 'snowRating' => 4, 'sunnyRating' => 10]]
+];
